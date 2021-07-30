91c7b9ca487010c7eab5fc46b95400825cc8ecad
layout <2534082481@qq.com>
2021-07-30 10:15:48 +0800

    first—实体

---
+++ JDProject/src/main/java/com/jd/shopping/entity/Content.java
@@ -0,0 +1,13 @@
+package com.jd.shopping.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Content {
+    private String title;
+    private String img;
+    private String price;
+}
